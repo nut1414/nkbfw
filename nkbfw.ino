51455065e9a84e8d9ce7b279a76ec1487dae592f
nut1414 <nut.pannapat@gmail.com>
2020-03-21 14:39:15 +0700

    Add Code

--- nkbfw.ino
+++ nkbfw.ino
@@ -30,19 +30,26 @@
 #define P9 10
 #define P10 11
 
+byte pin[KEYCOUNT]={P1,P2,P3,P4,P5,P6,P7,P8,P9,P10};
 
-byte pin[KEYCOUNT]=
-{     }
-
-bool test;
+//bool test;
+int i;
+Bounce button[KEYCOUNT];
 
 void setup() {
  //check if never been started before
- if(EEPROM.read(1023)== 255);
+ /*
+ if(EEPROM.read(1023)!= 1);
  {
   
  }
-  
+ */
+ //attach bounce
+  for (i = 0; i < KEYCOUNT; i++) {
+    button[i] = Bounce();                                     
+    button[i].attach(pin[i], INPUT_PULLUP); 
+    button[i].interval(5);
+  }
 
 //start the serial
 Serial.begin(9600);
@@ -50,8 +57,86 @@ pinMode(1,INPUT);
 }
 
 void loop() {
+  /*
 test = digitalRead(1);
 if(test==true){
   Serial.print("yes");
+  }*/
+for(i=0;i<KEYCOUNT;i++)
+  {
+  button[i].update();
   }
+
+  
+  if (button[0].fell()){
+    Keyboard.press(K1);
+  }
+  if (button[0].rose()){
+    Keyboard.release(K1);
+  }
+  
+if (button[1].fell()){
+    Keyboard.press(K2);
+  }
+  if (button[1].rose()){
+    Keyboard.release(K2);
+  }
+  
+if (button[2].fell()){
+    Keyboard.press(K3);
+  }
+  if (button[2].rose()){
+    Keyboard.release(K3);
+  }
+  
+if (button[3].fell()){
+    Keyboard.press(K4);
+  }
+  if (button[3].rose()){
+    Keyboard.release(K4);
+  }
+
+if (button[4].fell()){
+    Keyboard.press(K5);
+  }
+  if (button[4].rose()){
+    Keyboard.release(K5);
+  }
+
+if (button[5].fell()){
+    Keyboard.press(K6);
+  }
+  if (button[5].rose()){
+    Keyboard.release(K6);
+  }
+
+if (button[6].fell()){
+    Keyboard.press(K7);
+  }
+  if (button[6].rose()){
+    Keyboard.release(K7);
+  }
+
+if (button[7].fell()){
+    Keyboard.press(K8);
+  }
+  if (button[7].rose()){
+    Keyboard.release(K8);
+  }
+
+if (button[8].fell()){
+    Keyboard.press(K9);
+  }
+  if (button[8].rose()){
+    Keyboard.release(K9);
+  }
+
+if (button[9].fell()){
+    Keyboard.press(K10);
+  }
+  if (button[9].rose()){
+    Keyboard.release(K10);
+  }
+
+  
 }
