f3bfaadadd48915d69b78d803658dc3c68320fbb
nut1414 <nut.pannapat@gmail.com>
2020-04-19 20:34:56 +0700

    Serial Command Parsing, Fixed #if, Code Clean Up

--- nkbfw.ino
+++ nkbfw.ino
@@ -5,35 +5,36 @@
 #include <Keyboard.h>
 #include <Bounce2.h>
 
-//configuration
-#define HASLED FALSE
-#define LEDCHIPSET WS2812 //see FastLED list of supported chipset
+/*configuration*/
+
+//#define HASLED 
+#define LEDCHIPSET WS2812   //see FastLED list of supported chipset
 #define LEDCLORDER GRB
-#define LIGHTLEDONBOOT FALSE
+//#define LIGHTLEDONBOOT
 #define LEDPIN 16
 #define LEDCOUNT 0
-#define TURNONINBUILTLED FALSE
+#define TURNOFFINBUILTLED
 
-#define STARTSERIAL TRUE
+//#define STARTSERIAL
 #define KEYCOUNT 4
 
-//set your key matrix here
-#define XKEYMATRIX 4
-#define YKEYMATRIX 1
+/*set your key matrix here*/
+#define COLKEYMATRIX 4
+#define ROWKEYMATRIX 1
 
-//default pin of the switch
-const uint8_t colpin[XKEYMATRIX]=
+/*default pin of the switch*/
+const uint8_t colpin[COLKEYMATRIX]=
   {
     9,8,10,16
   };
-const uint8_t rowpin[YKEYMATRIX]=
+const uint8_t rowpin[ROWKEYMATRIX]=
   {
     NULL
   };
 
-//default key 
-//change your key binding here
-uint8_t defaultkey[XKEYMATRIX][YKEYMATRIX],key[XKEYMATRIX][YKEYMATRIX]=
+/*default key 
+change your key binding here*/
+uint8_t key[COLKEYMATRIX][ROWKEYMATRIX],defaultkey[COLKEYMATRIX][ROWKEYMATRIX]=
   {
     'z','x','c','v'
   };
@@ -41,24 +42,42 @@ uint8_t defaultkey[XKEYMATRIX][YKEYMATRIX],key[XKEYMATRIX][YKEYMATRIX]=
 
 //////////////////////////////////////////
 
+
+
 Bounce button[KEYCOUNT];
-#if HASLED == TRUE
+#ifdef HASLED 
 CRGB leds[LEDCOUNT];
 #endif
-
-int i,j,count;
-
+String serialstr;
+char parsechar[24],cmd[5];
+int i,j,count,crow,ccol;
+uint8_t ckey;
 void setup(){
   
  //begin keyboard
- Keyboard.begin();
- #if STARTSERIAL == TRUE
-  Serial.begin(9600);
- #endif
+Keyboard.begin();
+Serial.begin(9600);
+
+
+ 
+//attach bounce
+int buttoncount;
+ for (int k = 0; k < ROWKEYMATRIX ; k++) 
+ {
+  pinMode(rowpin[k],OUTPUT);
+  for (int l = 0; l < COLKEYMATRIX; l++) 
+  {
+    key[l][k] = defaultkey[l][k];
+    button[buttoncount] = Bounce();                                     
+    button[buttoncount].attach(colpin[l], INPUT_PULLUP); 
+    button[buttoncount].interval(5);
+    buttoncount++;
+  }
+ }
 
   
  //option to disable inbuilt led
- #if TURNONINBUILTLED == FALSE
+ #ifdef TURNOFFINBUILTLED
   pinMode(LED_BUILTIN, INPUT);
   pinMode(LED_BUILTIN_TX,INPUT);
   pinMode(LED_BUILTIN_RX,INPUT);
@@ -66,10 +85,10 @@ void setup(){
 
  
  //init led
- #if HASLED == TRUE
+ #ifdef HASLED 
   FastLED.addLeds<LEDCHIPSET,LEDPIN,LEDCLORDER>(leds,LEDCOUNT);
   
-  #if LIGHTLEDONBOOT == TRUE
+  #ifdef LIGHTLEDONBOOT
  
   for (int k = 0;k<LEDCOUNT;k++)
    {
@@ -83,38 +102,59 @@ void setup(){
  
 
 
-int buttoncount;
- //attach bounce
- for (int k = 0; k < YKEYMATRIX ; k++) 
- {
-  pinMode(rowpin[k],OUTPUT);
-  for (int l = 0; l < XKEYMATRIX; l++) 
+
+}
+
+
+//parsing the serial input
+void serialParser(String input)
+{
+  //converting serial.tostring to char array
+  input.toCharArray(parsechar,20);
+  
+  //scan for cmd
+  sscanf(parsechar,"%s %d %d %x",cmd, &ccol, &crow, &ckey);
+  
+  
+  if(strcmp(cmd,"CK")==0)
   {
-    button[buttoncount] = Bounce();                                     
-    button[buttoncount].attach(colpin[l], INPUT_PULLUP); 
-    button[buttoncount].interval(5);
-    buttoncount++;
+   key[ccol][crow] = ckey;
+   Serial.println(key[ccol][crow]);
+   
+  }else if(strcmp(cmd,"DK")==0)
+  {
+    for (int k = 0; k < ROWKEYMATRIX ; k++) 
+      {
+        
+        for (int l = 0; l < COLKEYMATRIX; l++) 
+          {
+            
+            key[l][k] = defaultkey[l][k];
+            
+          }
+      }
+  }else
+  {
+    Serial.println("INVALID");  
   }
- }
-
-
-
-//no use of the serial as of yet but definetly planned
-
-
+  
+  
 }
 
 void loop() {
  count = 0;
- for (j = 0; j < YKEYMATRIX; j++) 
+ for (j = 0; j < ROWKEYMATRIX; j++) 
  {
   digitalWrite(rowpin[j],HIGH);
-  for(i = 0; i < XKEYMATRIX; i++)
+  
+  for(i = 0; i < COLKEYMATRIX; i++)
     {
+      
       button[count].update();
       
       if (button[count].fell()){
        Keyboard.press(key[i][j]);
+       
      }
       if (button[count].rose()){
       Keyboard.release(key[i][j]);
@@ -123,4 +163,13 @@ void loop() {
     }
    digitalWrite(rowpin[j],LOW);
  }
+ 
+ while (Serial.available() > 0) 
+ {
+    serialstr = Serial.readString();
+  
+    serialParser(serialstr);
+ }
+
+ 
 }
